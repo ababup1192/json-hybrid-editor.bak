fcd1da8437707eda3317120f9b19ff7ce6c457da
ababup1192 <ababup1192@gmail.com>
2016-05-13 16:32:27 +0900

    Implement json tree

--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -7,6 +7,7 @@ import * as Bacon from 'baconjs'
 import VisualEditorApp from './visualEditorApp.tsx'
 import {HybridEditor} from './hybridEditor.ts'
 import TextEditor from './textEditor.ts'
+import {JsonTree} from './jsonTree.ts'
 
 module Main {
     export function main() {
@@ -21,8 +22,12 @@ module Main {
 
             appState.onValue((state: { json: Object }) => {
                 console.log(state);
+                
                 ReactDOM.render(<VisualEditorApp {...state}/>, document.getElementById('visual-editor'));
+
+                JsonTree.view();
             });
+
         });
     }
 }
